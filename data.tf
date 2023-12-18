60e7f8d5ef561c93e1be20166593c6b490be53b4
internetuser2008 <internetuser2008@yahoo.com>
2023-12-18 06:21:44 -0600

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_eks_cluster" "dev-cluster" {
-  name = module.my-cluster.cluster_id
+  name = module.wordpressCL.cluster_id
 }
 
 data "aws_eks_cluster_auth" "dev-cluster" {
-  name = module.my-cluster.cluster_id
+  name = module.wordpressCL.cluster_id
 }
